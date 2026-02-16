ec5482b4189d91b285f9e1ab711115dc71fba17c
Lushe Shipkov <71821057+anarchvile@users.noreply.github.com>
2026-02-15 22:23:48 -0800

    More CI fixes. (#3)
    * More CI fixes.
    
    * Remove unnecessary comment.

--- crates/template/src/main.rs
+++ crates/template/src/main.rs
@@ -3,10 +3,8 @@
 use template::add;
 
 /// Entry-point to the binary crate.
-// GCOVR_EXCL_START
 fn main() {
     let a = 5;
     let b = 4;
     println!("{} + {} = {}", a, b, add(a, b));
 }
-// GCOVR_EXCL_STOP
